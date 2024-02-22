e7ee7e4710d6aa7095c112aa73a1d7b2ab08cd57
RahulK <rahulkannan1605@gmail.com>
2024-02-22 18:40:33 +0530

    added monitoring and logging

--- my_1st_app/settings.py
+++ my_1st_app/settings.py
@@ -136,3 +136,22 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+   "version": 1,
+   "disable_existing_loggers": False,
+   "handlers": {
+       "file": {
+           "level": "DEBUG",
+           "class": "logging.FileHandler",
+           "filename": "/home/rahulkannan1605/debug.log",
+       },
+   },
+   "loggers": {
+       "django": {
+           "handlers": ["file"],
+           "level": "DEBUG",
+           "propagate": True,
+       },
+   },
+}
